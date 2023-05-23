92a3a16897520b984cbe9532399ba45600172fdf
Akashiutchiha <christianhonore2003@gmail.com>
2023-05-23 23:45:30 +0100

    feat: LibraryCard entity created

--- everGreen/library/models.py
+++ everGreen/library/models.py
@@ -27,3 +27,27 @@ class User(models.Model):
     def __str__(self):
         return self.username
     
+class LibraryCard(models.Model):
+    card_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    issuedDate = models.DateField(auto_now_add=True)
+    returnedDate = models.DateField()
+    is_returned = models.BooleanField(default=False)
+    cardNumber = models.IntegerField()
+    
+    def __str__(self):
+        return self.cardNumber
+    
+class Borrowing(models.Model):
+    borrowing_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    card_id = models.ForeignKey(LibraryCard, on_delete=models.CASCADE)
+    borrowDate = models.DateField(auto_now_add=True)
+    date_due = models.DateField()
+    is_returned = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.borrowing_id
+    
+    
