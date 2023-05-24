6ecba0953c2cc59c80f85846766f6bca9c04b9f9
Akashiutchiha <christianhonore2003@gmail.com>
2023-05-24 22:51:48 +0100

    feat: Register functionaliyt OK

--- everGreen/library/views.py
+++ everGreen/library/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from . import models
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from .serializer import *
 
@@ -17,6 +18,13 @@ from .serializer import *
 #     data = models.Book.objects.all()
 #     serializer = BookSerializer(data, many=True)
 #     return Response(serializer.data)
+
+
+{
+"username":"neil2",
+"password":"123456",
+"email":"christiangonore2003@gmail.com"
+}
 
 class UserListView(APIView):
     authentication_classes = [authentication.TokenAuthentication]
@@ -34,6 +42,7 @@ class RegisterView(APIView):
             email = serializer.validated_data['email']
             password = serializer.validated_data['password']
             user = User.objects.create_user(username=username, email=email, password=password)
+            token = Token.objects.create(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
