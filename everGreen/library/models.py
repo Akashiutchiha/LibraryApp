30613cbee44aa6bd0b19dd1fe0ce202582839082
Akashiutchiha <christianhonore2003@gmail.com>
2023-05-24 21:55:16 +0100

    feat: authentication Ok

--- everGreen/library/models.py
+++ everGreen/library/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, *args, **kwargs,):
+    if created:
+        Token.objects.create(user=instance)
+    
+
 
 
 # Create your models here.
